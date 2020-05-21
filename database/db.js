7bef203932736cf0479c9c3643febf6b245eae97
Chris Sanchez <chrissanchez89@gmail.com>
2020-05-21 09:39:39 -0700

    Comments out mysql DB to get project working

--- database/db.js
+++ database/db.js
@@ -1,23 +1,23 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'chris',
-  database: 'fenty'
-});
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'chris',
+//   database: 'fenty'
+// });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ', err);
-    return;
-  }
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ', err);
+//     return;
+//   }
+// });
 
-// No Clue why this was here, I think it's to check that the DB is functioning.
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution to 1 + 1 is: ', results[0].solution);
-});
+// // No Clue why this was here, I think it's to check that the DB is functioning.
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution to 1 + 1 is: ', results[0].solution);
+// });
 
 
-module.exports = connection;
+// module.exports = connection;
