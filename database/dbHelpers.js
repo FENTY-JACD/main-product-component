19abd982676850b097b57ba2fb6e4ba7c863fce5
Chris Sanchez <chrissanchez89@gmail.com>
2020-05-23 14:29:12 -0700

    Creates the /GET route to PSQL DB

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -1,45 +1,45 @@
-const db = require('./db.js')
+const pool = require('./postgresDB.js')
 
 
 const dbHelpers = {
   // get product
-  get: (callback) => {
-    db.query(`SELECT * FROM products;`, (err, results) => {
-      if(err) {
-        callback(err)
-      } else {
-        callback(null, results)
-      }
-    })
-  },
-  getPhotos: (id, callback) => {
-    //
-    let queryStr = `select products.id, products.name, photos.background, photos.img1, photos.img2, photos.img3 from photos left outer join products on products.id = photos.id;`
-    db.query(queryStr, (err, results) => {
-      if(err) {
-        callback(err)
-      } else {
-        callback(null, results)
-      }
-    })
-  },
-  getColors: (callback) => {
-    let queryStr = `select products.id, products.name, colors.name1, colors.name2, colors.name3, colors.color1, colors.color2, colors.color3  from colors inner join products on products.id = colors.id`
-    db.query(queryStr, (err, results) => {
-      if(err) {
-        callback(err)
-      } else {
-        callback(null, results)
-      }
-    })
-  },
+  // get: (callback) => {
+  //   db.query(`SELECT * FROM products;`, (err, results) => {
+  //     if(err) {
+  //       callback(err)
+  //     } else {
+  //       callback(null, results)
+  //     }
+  //   })
+  // },
+  // getPhotos: (id, callback) => {
+  //   //
+  //   let queryStr = `select products.id, products.name, photos.background, photos.img1, photos.img2, photos.img3 from photos left outer join products on products.id = photos.id;`
+  //   db.query(queryStr, (err, results) => {
+  //     if(err) {
+  //       callback(err)
+  //     } else {
+  //       callback(null, results)
+  //     }
+  //   })
+  // },
+  // getColors: (callback) => {
+  //   let queryStr = `select products.id, products.name, colors.name1, colors.name2, colors.name3, colors.color1, colors.color2, colors.color3  from colors inner join products on products.id = colors.id`
+  //   db.query(queryStr, (err, results) => {
+  //     if(err) {
+  //       callback(err)
+  //     } else {
+  //       callback(null, results)
+  //     }
+  //   })
+  // },
 
   ////////// Here starts the work of Chris, come to save the day ////////
 
   getProduct: (id, callback) => {
-    let queryStr = ``;
+    let queryStr = `SELECT * FROM products WHERE id = ${id}`;
 
-    db.query(queryStr, (err, results) => {
+    pool.query(queryStr, (err, results) => {
       if(err) {
         callback(err)
       } else {
@@ -48,45 +48,41 @@ const dbHelpers = {
     })
   },
 
-  postProduct: (params, callback) => {
-    let queryStr = ``;
+  // postProduct: (params, callback) => {
+  //   let queryStr = ``;
 
-    db.query(queryStr, (err, results) => {
-      if(err) {
-        callback(err)
-      } else {
-        callback(null, results)
-      }
-    })
-  },
+  //   db.query(queryStr, (err, results) => {
+  //     if(err) {
+  //       callback(err)
+  //     } else {
+  //       callback(null, results)
+  //     }
+  //   })
+  // },
 
-  updateProduct: (params, callback) => {
-    let queryStr = ``;
+  // updateProduct: (params, callback) => {
+  //   let queryStr = ``;
 
-    db.query(queryStr, (err, results) => {
-      if(err) {
-        callback(err)
-      } else {
-        callback(null, results)
-      }
-    })
-  },
+  //   db.query(queryStr, (err, results) => {
+  //     if(err) {
+  //       callback(err)
+  //     } else {
+  //       callback(null, results)
+  //     }
+  //   })
+  // },
 
-  deleteProduct: (id, callback) => {
-    let queryStr = `DELETE products, colors, photos FROM products INNER JOIN colors INNER JOIN photos ON t2.ref = t1.id WHERE t1.id = 1;`;
+  // deleteProduct: (id, callback) => {
+  //   let queryStr = `DELETE products, colors, photos FROM products INNER JOIN colors INNER JOIN photos ON t2.ref = t1.id WHERE t1.id = 1;`;
 
-    db.query(queryStr, (err, results) => {
-      if(err) {
-        callback(err)
-      } else {
-        callback(null, results)
-      }
-    })
-  },
-
-
-
-
+  //   db.query(queryStr, (err, results) => {
+  //     if(err) {
+  //       callback(err)
+  //     } else {
+  //       callback(null, results)
+  //     }
+  //   })
+  // },
 
 
 }
--- server/controller.js
+++ server/controller.js
@@ -1,43 +1,44 @@
 const dbHelpers = require('../database/dbHelpers.js')
 
 const controller = {
-  get: (req, res) => {
-    dbHelpers.get((err, results) => {
-      if(err) {
-        res.status(400).send(err)
-      } else {
-        res.status(200).send(results)
-      }
-    })
-  },
+  // get: (req, res) => {
+  //   dbHelpers.get((err, results) => {
+  //     if(err) {
+  //       res.status(400).send(err)
+  //     } else {
+  //       res.status(200).send(results)
+  //     }
+  //   })
+  // },
 
-  getPhotos: (req, res) => {
-    const {id} = req.params
+  // getPhotos: (req, res) => {
+  //   const {id} = req.params
 
-    dbHelpers.getPhotos(id, (err, results) => {
-      if(err) {
-        res.status(400).send(err)
-      } else {
-        res.status(200).send(results)
-      }
-    })
-  },
+  //   dbHelpers.getPhotos(id, (err, results) => {
+  //     if(err) {
+  //       res.status(400).send(err)
+  //     } else {
+  //       res.status(200).send(results)
+  //     }
+  //   })
+  // },
 
-  getColors: (req, res) => {
-    dbHelpers.getColors((err, results) => {
-      if(err) {
-        res.status(400).send(err)
-      } else {
-        res.status(200).send(results)
-      }
-    })
-  },
+  // getColors: (req, res) => {
+  //   dbHelpers.getColors((err, results) => {
+  //     if(err) {
+  //       res.status(400).send(err)
+  //     } else {
+  //       res.status(200).send(results)
+  //     }
+  //   })
+  // },
 
 ///// Here starts the work of Chris ///////
 
   getProduct: (req, res) => {
-
-    dbHelpers.getproduct(req.params, (err, results) => {
+    let id = req.params.id;
+    console.log(id);
+    dbHelpers.getProduct(id, (err, results) => {
       if(err) {
         res.status(400).send(err)
       } else {
@@ -46,35 +47,35 @@ const controller = {
     })
   },
 
-  postProduct: (req, res) => {
-    dbHelpers.postProduct(req.params, (err, results) => {
-      if(err) {
-        res.status(400).send(err)
-      } else {
-        res.status(200).send(results)
-      }
-    })
-  },
+  // postProduct: (req, res) => {
+  //   dbHelpers.postProduct(req.params, (err, results) => {
+  //     if(err) {
+  //       res.status(400).send(err)
+  //     } else {
+  //       res.status(200).send(results)
+  //     }
+  //   })
+  // },
 
-  updateProduct: (req, res) => {
-    dbHelpers.updateProduct(req.params, (err, results) => {
-      if(err) {
-        res.status(400).send(err)
-      } else {
-        res.status(200).send(results)
-      }
-    })
-  },
+  // updateProduct: (req, res) => {
+  //   dbHelpers.updateProduct(req.params, (err, results) => {
+  //     if(err) {
+  //       res.status(400).send(err)
+  //     } else {
+  //       res.status(200).send(results)
+  //     }
+  //   })
+  // },
 
-  deleteProduct: (req, res) => {
-    dbHelpers.deleteProduct(req.params, (err, results) => {
-      if(err) {
-        res.status(400).send(err)
-      } else {
-        res.status(200).send(results)
-      }
-    })
-  }
+  // deleteProduct: (req, res) => {
+  //   dbHelpers.deleteProduct(req.params, (err, results) => {
+  //     if(err) {
+  //       res.status(400).send(err)
+  //     } else {
+  //       res.status(200).send(results)
+  //     }
+  //   })
+  // }
 
 
 }
