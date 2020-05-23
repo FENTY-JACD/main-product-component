98acde5e9fa02f1d13d7a95c8222568f66cb4afe
Chris Sanchez <chrissanchez89@gmail.com>
2020-05-23 14:30:50 -0700

    adds speed test to directory

---
+++ tests/k6Script.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+
+export let options = {
+  vus: 1000,
+  duration: '10s',
+};
+
+export default function() {
+  http.get('http://localhost:8080/api/product/9999000');
+  sleep(1);
+}
+
+
